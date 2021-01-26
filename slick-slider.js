56a49ddf15ab29c300492709995e022eacf8517a
mazurenkooleksandr <kakchtoinfo@ukr.net>
2021-01-27 01:33:29 +0200

    commit

--- slick-slider.js
+++ slick-slider.js
@@ -1,7 +1,20 @@
-$('.third-container__slides').slick({
-    infinite: true,
-    dots: true,
-    slidesToShow: 3,
-    slidesToScroll: 1
-    
-  });
+$(document).ready(function(){
+    $('.third-container__slides').slick({
+        arrows: true,
+        dots: true,
+        slidesToShow: 3,
+
+    });
+});
+
+$(document).ready(function(){
+    $('.first-container__block').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 1,
+
+    });
+});
+
+
+       
